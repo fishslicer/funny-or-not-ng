f6ed1daeac221fe5c49139aa3dc9b4cc8c399121
fishslicer <rmann3@mail.csuchico.edu>
2018-06-04 17:44:58 -0700

    FON-4 added selected showcase at top

--- src/app/mock-media-objects.ts
+++ src/app/mock-media-objects.ts
@@ -5,5 +5,5 @@ export const MEDIA_OBJECTS: MediaObject[] = [
     {title: 'This is really bad', desc: '8u98fhjdoijansdf lasdfjkajklj kajlkfdsalk jklfjklajk', imgUrl: 'http://pm1.narvii.com/6299/72d10b254f0234e91f9e0ba0f431276df49ea4e1_00.jpg'},
     {title: 'LOL', desc: 'asfdkjasdfjlkjadfls dlkadhfsioas hnflajsdfkjasj lkdfuu fjd', imgUrl: 'http://pm1.narvii.com/6299/72d10b254f0234e91f9e0ba0f431276df49ea4e1_00.jpg'},
     {title: 'Memes', desc: 'asdiofioasdjilkasld fjlkafsdfjklasdjk lfdjlkal ksjflkj asdf', imgUrl: 'http://pm1.narvii.com/6299/72d10b254f0234e91f9e0ba0f431276df49ea4e1_00.jpg'},
-    {title: 'Blank title here', desc: 'asjl kklfal f kjfdjlkajkl sflkjas klfkljhjkas suhkdfhjl kfas dhklj adfs', imgUrl: 'http://pm1.narvii.com/6299/72d10b254f0234e91f9e0ba0f431276df49ea4e1_00.jpg'}
+    {title: 'Blank title here', desc: 'asjl kklfal f kjfdjlkajkl sflkjas klfkljhjkas suhkdfhjl kfas dhklj adfs', imgUrl: 'http://pm1.narvii.com/6299/72d10b254f0234e91f9e0ba0f431276df49ea4e1_00.jpg'},
 ]
